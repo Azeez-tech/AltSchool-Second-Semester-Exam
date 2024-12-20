37099b32eba333635fe11b4129412f5755986ca0
Muhammed Azeez <muhammedazeez09@gmail.com>
2024-12-20 12:31:51 +0100

    I added open port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const JWT_SECRET = process.env.JWT_SECRET
 
 app.listen(PORT, () => {
